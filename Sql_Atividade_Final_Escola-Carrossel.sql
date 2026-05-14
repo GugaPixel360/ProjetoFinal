c850670bc7d85f4d26d916e632f9f3fe84128af0
GugaPixel360 <gmadureira198@gmail.com>
2026-05-14 16:07:38 -0300

    criei e ajeitei coisas (explicado dec)
    criei a permanencia das caracteristicas do professor e arunei alguns erros no mysql e basicos no python

--- Sql_Atividade_Final_Escola-Carrossel.sql
+++ Sql_Atividade_Final_Escola-Carrossel.sql
@@ -3,18 +3,17 @@ CREATE DATABASE IF NOT EXISTS Carrossel;
 USE carrossel;
 
 CREATE TABLE IF NOT EXISTS Alunos (
-nomeDoAluno VARCHAR (100) NOT NULL,
-idadeDoAluno INT NOT NULL,
-turmaDoAluno INT NOT NULL,
+nome_Aluno VARCHAR (100) NOT NULL,
+idade_Aluno INT NOT NULL,
+turma_Aluno INT NOT NULL,
 matricula_ID INT AUTO_INCREMENT PRIMARY KEY,
-notasDoAluno INT,
-oqvce VARCHAR (100) NOT NULL
+notas_Aluno INT
 );
 
 CREATE TABLE IF NOT EXISTS Situacao (
-notasDoAluno_FK INT,
-mediaDoAluno INT,
-situacaoDoAluno VARCHAR (100),
+notas_Aluno_FK INT,
+media_Aluno INT,
+situacao_Aluno VARCHAR (100),
 matricula_FK_ID INT AUTO_INCREMENT PRIMARY KEY,
 FOREIGN KEY (Matricula_FK_ID) REFERENCES Alunos (Matricula_ID)
 
@@ -22,10 +21,11 @@ FOREIGN KEY (Matricula_FK_ID) REFERENCES Alunos (Matricula_ID)
 
 CREATE TABLE IF NOT EXISTS professor (
 id_docente int auto_increment primary key,  
-nomeDoprofessor VARCHAR (100) NOT NULL,
-funcaoDoProfessor INT NOT NULL,             
-emailDoProfessor INT NOT NULL,
-senha VARCHAR(15) NOT NULL,
+nome_professor VARCHAR (100) NOT NULL,
+funcao_Professor INT NOT NULL,             
+email_Professor INT NOT NULL,
+senha VARCHAR (15) NOT NULL,
+materia_Professor VARCHAR (100) NOT NULL
 );
 
 
