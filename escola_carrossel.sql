efff14ce30ef8f1219a3e38693050775752ba7a6
Josuejose11 <josuejose11@outlook.com>
2026-05-14 16:13:14 -0300

    Add files via upload

---
+++ escola_carrossel.sql
@@ -0,0 +1,34 @@
+CREATE DATABASE IF NOT EXISTS Carrossel;
+
+USE carrossel;
+
+CREATE TABLE IF NOT EXISTS Alunos (
+nome_aluno VARCHAR (100) NOT NULL,
+idade_aluno INT NOT NULL,
+turma_aluno INT NOT NULL,
+matricula_ID INT AUTO_INCREMENT PRIMARY KEY,
+notas_aluno INT
+);
+
+CREATE TABLE IF NOT EXISTS Situacao (
+notas_aluno_FK INT,
+media_aluno INT,
+situacao_aluno VARCHAR (100),
+matricula_FK_ID INT AUTO_INCREMENT PRIMARY KEY,
+FOREIGN KEY (Matricula_FK_ID) REFERENCES Alunos (Matricula_ID)
+
+);
+
+CREATE TABLE IF NOT EXISTS professor (
+id_docente int auto_increment primary key,  
+nome_docente VARCHAR (100) NOT NULL,
+funcao_docente INT NOT NULL,             
+email_docente INT NOT NULL,
+senha VARCHAR (15) NOT NULL,
+materia_docente VARCHAR (100) NOT NULL
+);
+
+
+/*
+Drop Database Carrossel;
+
