572b787d8056024397bedf7ef93427e614cc10e3
GugaPixel360 <gmadureira198@gmail.com>
2026-05-29 09:12:26 -0300

    errinho

--- defs.py
+++ defs.py
@@ -786,6 +786,7 @@ def media(matricula):
                 print("Status: Recuperação")
         else:
             print(f"Nenhuma nota encontrada para o aluno com ID {matricula}.")
+        break
 
 
 #DEFs LER
