bc845299bb49a5802c13331a857e2660cd9b695a
GugaPixel360 <gmadureira198@gmail.com>
2026-05-29 08:46:43 -0300

    erros denovo

--- defs.py
+++ defs.py
@@ -704,36 +704,42 @@ def ler_alunos():
 #retorna a funcao do docente
 def ler_funcao(id_docente):
     conn = criar_conexao()
+
+    if conn is None:
+        return
+
     cursor = conn.cursor()
-    sql = "SELECT * FROM professor WHERE id_docente = %s"
+
+    sql = """
+    SELECT funcao_docente
+    FROM professor
+    WHERE id_docente = %s
+    """
 
     cursor.execute(sql, (id_docente,))
 
     resultado = cursor.fetchone()
 
+    cursor.close()
+    conn.close()
 
-    sql = "SELECT funcao_docente FROM professor WHERE id_docente = %s"
-    
-    resultado = cursor.fetchone()
+    if resultado is None:
+        print("ID não encontrado")
+        return
 
     funcao_docente = resultado[0]
 
-    
-    if resultado:
-        print(f"Seja bem vindo(a) {funcao_docente}")
-        match funcao_docente:
-            case "professor":
-                return 1
-            case "coordenador":
-                return 2
-            case "diretor":
-                return 3
+    print(f"Seja bem vindo(a) {funcao_docente}")
 
-    else:
-        print(f"O valor '{id_docente}' NÃO foi encontrado.")
-    
-    cursor.close()
-    conn.close()
+    match funcao_docente:
+        case "professor":
+            return 1
+
+        case "coordenador":
+            return 2
+
+        case "diretor":
+            return 3
 
 #ler notas
 def ler_notas(matricula):
