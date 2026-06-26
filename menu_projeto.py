cc49c2af27680bbeaa8744fc9f08695fca34c779
Josuejose11 <josuejose11@outlook.com>
2026-06-26 11:39:09 -0300

    commit final

--- menu_projeto.py
+++ menu_projeto.py
@@ -119,11 +119,6 @@ while True:
 
                 denovo()
 
-        case "67":
-            while True:
-                print("NÃO PODE 67 NA AULA DO BRUNO!!!!!!")
-                break
-
         case _:
             erro()
             print("Escolha uma das opções dadas")
